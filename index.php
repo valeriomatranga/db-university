8a8078a65ccb38b3bd7e70a20e1aaae73f9e8073
valeriomatranga <78118886+valeriomatranga@users.noreply.github.com>
2021-07-18 13:05:08 +0200

    correzione

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
+
 //definite le constanti
 define('DB_SERVERNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'univerity');
 define('DB_PORT', '3306');
-//var_dump(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+
 
 //stabilita la connessione al database
 $connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
-//var_dump($connection);
 
 
 // verifica la connessione
@@ -17,20 +17,17 @@ if($connection && $connection->connect_error){
     echo 'connection failed: ' . $connection->connect_error;
     die();
 }
-//echo 'Connection Successeful!';
 
 
 //creata una richiesta query
 $sql = "SELECT `students`.`name`,`students`.`surname` FROM `students` WHERE YEAR(`date_of_birth`)=1990";
-//var_dump($sql);
 $results = $connection->query($sql);
-//var_dump($results);
 
 
 //stampa tramite ciclo while i risultati della query
 if($results && $results->num_rows > 0){
-    while($student = $results->fetch_assoc()){
-    ?>
+    while($student = $results->fetch_assoc()){ ?>
+
     <ul>
         <li>
             <div>
@@ -39,6 +36,7 @@ if($results && $results->num_rows > 0){
             </div>
         </li>
     </ul>
+    
    <?php 
     }
 }elseif($results){
@@ -47,5 +45,6 @@ if($results && $results->num_rows > 0){
     echo 'Errore Query';
 }
 
+//chiude la connessione al database
 $connection->close();
 ?>
